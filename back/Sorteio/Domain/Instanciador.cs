f16f9fb9ff23abcd2687d2d4d818f7689c72b181
Lucas Costa <slucascosta@hotmail.com>
2018-10-11 04:20:34 -0300

    master

---
+++ back/Sorteio/Domain/Instanciador.cs
@@ -0,0 +1,26 @@
+﻿using Sorteio.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sorteio.Domain
+{
+    public class Instanciador
+    {
+        const string nameSpace = "Sorteio.Domain.Business.{0}Controller";
+
+        public static ModeloAposta GerarIntancia(string tipo)
+        {
+            Type type = Type.GetType(string.Format(nameSpace, tipo));
+
+            if(type == null)
+            {
+                throw new Exception(
+                    string.Concat("Modelo de aposta não encontrado: ", tipo));
+            }
+
+            return (ModeloAposta)Activator.CreateInstance(type);
+        }
+    }
+}
---
+++ back/Sorteio/Models/Aposta.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sorteio.Models
+{
+    public class Aposta
+    {
+        string modeloAposta;
+
+        public Aposta(string modeloAposta)
+        {
+            this.modeloAposta = modeloAposta;
+        }
+
+        public int Id { get; set; }
+
+        [JsonProperty("Data")]
+        public string Registro
+        {
+            get
+            {
+                return this.Data.ToString("dd/MM/yyyy HH:mm");
+            }
+        }
+
+        [JsonProperty("Numero")]
+        public string DescNumero
+        {
+            get
+            {
+                return Join(this.Numero);
+            }
+        }
+
+        public string ModeloAposta { get { return modeloAposta; } }
+
+        [JsonIgnore]
+        public DateTime Data { get; set; }
+
+        [JsonIgnore]
+        public int[] Numero { get; set; }
+
+        public static string Join(IEnumerable<int> numero)
+        {
+            string join = string.Join(", ",
+                numero.Select(x => x.ToString()));
+
+            return join;
+        }
+
+        public bool Comparar(int[] numero)
+        {
+            var orderThis = this.Numero.OrderBy(x => x);
+            var orderNum = numero.OrderBy(x => x);
+
+            return Join(orderThis) == Join(orderNum);
+        }
+    }
+}
