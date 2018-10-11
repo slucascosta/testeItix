e0bef72e301b5f122b6bd5bf7b6f03d2ab464c01
Lucas Costa <slucascosta@hotmail.com>
2018-10-11 04:03:39 -0300

    Initial commit

---
+++ back/Sorteio/Domain/Business/MegaSenaController.cs
@@ -0,0 +1,76 @@
+﻿using Sorteio.Domain.Interface;
+using Sorteio.Domain.Singleton;
+using Sorteio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sorteio.Domain.Business
+{
+    public class MegaSenaController : ModeloAposta
+    {
+        public override void Configurar()
+        {
+            Modelo = "MEGASENA";
+            QuantDezena = 6;
+            MinDezena = 1;
+            MaxDezena = 60;
+        }
+
+        public override List<int> GerarNumero()
+        {
+            List<int> numero = new List<int>();
+            Random ran = new Random();
+
+            while (numero.Count < QuantDezena)
+            {
+                int num = ran.Next(60);
+
+                if (!numero.Contains(num) && num >= MinDezena && num <= MaxDezena)
+                {
+                    numero.Add(num);
+                }
+            }
+
+            if (SingApostas.Instance.VerificarDuplicidade(numero.ToArray(), Modelo))
+            {
+                numero = GerarNumero();
+            }
+
+            return numero;
+        }
+
+        public override List<Aposta> ObterApostas()
+        {
+            List<Aposta> apostas = SingApostas.Instance.ObterApostas(Modelo);
+
+            return apostas;
+        }
+
+        public override Aposta Registrar(int[] numero)
+        {
+            Validar(numero);
+
+            Aposta aposta = SingApostas.Instance.Registrar(numero, Modelo);
+
+            return aposta;
+        }
+
+        public override List<Aposta> Sortear(string tipo, int id)
+        {
+            if (tipo == "Quina")
+            {
+                return SingApostas.Instance.ObterDezenasIguais(Modelo, id, 5);
+            }
+            else if (tipo == "Quadra")
+            {
+                return SingApostas.Instance.ObterDezenasIguais(Modelo, id, 4);
+            }
+            else
+            {
+                throw new Exception(string.Concat("Sub tipo inválido: ", tipo));
+            }
+        }
+    }
+}
---
+++ back/Sorteio/Domain/Interface/IModeloAposta.cs
@@ -0,0 +1,88 @@
+﻿using Sorteio.Domain.Singleton;
+using Sorteio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteio.Domain.Interface
+{
+    public interface IModeloAposta
+    {
+        List<int> GerarNumero();
+        Aposta Registrar(int[] numero);
+        List<Aposta> ObterApostas();
+        void Validar(int[] numero);
+        List<Aposta> Sortear(string tipo, int id);
+    }
+
+    public abstract class ModeloAposta : IModeloAposta
+    {
+        public string Modelo;
+        public int QuantDezena;
+        public int MinDezena;
+        public int MaxDezena;
+
+        public ModeloAposta()
+        {
+            Configurar();
+        }
+
+        public abstract void Configurar();
+        public abstract List<int> GerarNumero();
+        public abstract Aposta Registrar(int[] numero);
+        public abstract List<Aposta> ObterApostas();
+
+        public void Validar(int[] numero)
+        {
+            if (numero.Length != QuantDezena)
+            {
+                throw new Exception(
+                    string.Format("As apostas devem compostas por {0} números.", QuantDezena));
+            }
+
+            ValidarDezenas(numero);
+            ValidarDuplicidade(numero);
+        }
+
+        private void ValidarDezenas(int[] numero)
+        {
+            for (int i = 0; i < numero.Length; i++)
+            {
+                if (numero[i] < MinDezena || numero[i] > MaxDezena)
+                {
+                    throw new Exception(
+                        string.Format("O valor das dezenas devem estar entre {0} e {1}.", MinDezena, MaxDezena));
+                }
+
+                if (i + 1 == QuantDezena)
+                {
+                    return;
+                }
+
+                for (int j = i + 1; j < numero.Length; j++)
+                {
+                    if (numero[i] == numero[j])
+                    {
+                        throw new Exception(
+                            string.Concat("Não pode haver dezena repetida: ", string.Join(", ", numero)));
+                    }
+                }
+            }
+        }
+
+        private void ValidarDuplicidade(int[] numero)
+        {
+            bool booDuplicidade = SingApostas.Instance.VerificarDuplicidade(numero, Modelo);
+
+            if (booDuplicidade)
+            {
+                throw new Exception(
+                    string.Concat("Não é permitido apostar jogo repetido: ", string.Join(", ", numero)));
+            }
+        }
+
+        public abstract List<Aposta> Sortear(string tipo, int id);
+    }
+}
